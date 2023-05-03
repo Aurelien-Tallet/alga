5404dfc2590544effd6920d0a8ea13b9426e0154
Aurelien-Tallet <aurelien.jarte@gmail.com>
2023-05-03 15:01:21 +0200

    comment main for readabity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         StringPatternSearchTest.run();
+
+        // TEST AHO CORASICK WITH MULTIPLE PATTERNS (RETURN ALL PATTERN PRESENT IN THE TEXT)
         String[] P = {"he", "she", "his", "hers"} ;
         String T = "HERE IS A SIMPLE EXAMPLE";
         AhoCorasick ahoCorasick = new AhoCorasick(Arrays.asList(P));
